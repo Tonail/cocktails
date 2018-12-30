0fec2a7b94be6c9369bb722269450cc050b5a9bf
tonail <tbush01@gmail.com>
2018-12-30 12:46:37 -0600

    fix path

--- src/Containers/Contact/index.js
+++ src/Containers/Contact/index.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './contact.less'
-import '../../globals.less'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./contact.less";
 
 export default class Contact extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return <div>Contact</div>
-  }
+	render() {
+		return <div>Contact</div>;
+	}
 }
